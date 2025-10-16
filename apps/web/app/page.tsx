51bf4747683f1d6e4042fd7c3111c1fcdf3754c3
Jason <jason@jason.com>
2025-10-16 10:13:06 +0530

    remove unwanted imports

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -2,7 +2,6 @@
 import { useMutation, useQuery } from "convex/react";
 import { api } from "@workspace/backend/_generated/api";
 import { Button } from "@workspace/ui/components/button";
-import { add } from "@workspace/backend/users";
 export default function Page() {
   const users = useQuery(api.users.getMany);
   const addUser = useMutation(api.users.add);
