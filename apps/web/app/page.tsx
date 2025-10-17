767610b97ee96ed87c0ff0523bb06b5f4ba6fcef
Jason <jason@jason.com>
2025-10-17 10:36:55 +0530

    03:Clerk Integration

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,27 +1,45 @@
 "use client";
-import { useMutation, useQuery } from "convex/react";
+import { Authenticated, Unauthenticated, useMutation, useQuery } from "convex/react";
 import { api } from "@workspace/backend/_generated/api";
 import { Button } from "@workspace/ui/components/button";
-import { add } from "@workspace/backend/users";
+import { SignInButton, UserButton } from "@clerk/nextjs";
 export default function Page() {
   const users = useQuery(api.users.getMany);
   const addUser = useMutation(api.users.add);
+  if (!users) {
+    return <div>Loading....</div>;
+  }
   return (
-    <div className="flex  flex-col gap-y-6 items-center justify-center my-10">
-      <h1 className="text-2xl font-bold">apps/Web</h1>
-      <div className="flex gap-y-6">
-        <p className="flex flex-col gap-6">
-          {users?.map((user) => (
-            <p className="flex flex-col">{user.name}</p>
-          ))}
-        </p>
-      </div>
-      <Button
-        onClick={() => {
-          addUser();
-        }}>
-        Add
-      </Button>
-    </div>
+    <>
+      <Unauthenticated>
+        <div className="flex flex-col gap-y-4 mx-4">
+          <h1 className="flex">Please Login!</h1>
+          <Button className="flex max-w-sm">
+            <SignInButton />
+          </Button>
+        </div>
+      </Unauthenticated>
+      <Authenticated>
+        <div className="flex  flex-col gap-y-6 items-center justify-center my-10">
+          <h1 className="text-2xl font-bold">apps/Web</h1>
+          <div className="flex gap-y-6">
+            <div className="flex flex-col gap-6">
+              {users.map((user) => (
+                <p className="flex flex-col" key={users._id}>
+                  {user.name}
+                </p>
+              ))}
+            </div>
+          </div>
+          <Button
+            onClick={() => {
+              addUser();
+            }}>
+            Add
+          </Button>
+        </div>
+        <UserButton />
+      </Authenticated>
+    </>
   );
 }
--- apps/widget/app/page.tsx
+++ apps/widget/app/page.tsx
@@ -1,13 +1,18 @@
 "use client";
 
-import { useQuery } from "convex/react";
+import { useMutation, useQuery } from "convex/react";
 import { api } from "@workspace/backend/_generated/api";
+import { Button } from "@workspace/ui/components/button";
 export default function Page() {
   const users = useQuery(api.users.getMany);
+  const addUser = useMutation(api.users.add);
   return (
-    <div className="flex items-center justify-center my-10">
+    <div className="flex  flex-colitems-center justify-center my-10">
       <h1 className="text-2xl font-bold">apps/Web</h1>
-      <p>{users?.map((user) => user.name)}</p>
+      <h2>{users?.map((user) => user.name)}</h2>
+      <div>
+        <Button onClick={() => addUser()}>Add User</Button>
+      </div>
     </div>
   );
 }
